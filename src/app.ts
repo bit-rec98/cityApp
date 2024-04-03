69f2eb1fbdb3c904c473d4f716fe235b6ef72836
0xtian.js <bitrec.61@gmail.com>
2024-04-03 12:05:49 -0300

    Started project development

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import './styles/navBar.css'
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+      families: ['Exo', 'sans-serif'], // Reemplaza con las fuentes que elijas
+    },
+});
+
+
+
