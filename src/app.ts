b6b7f5abcf7c0b730351c56317d909230046560f
0xtian.js <bitrec.61@gmail.com>
2024-04-10 16:13:49 -0300

    Added responsive displaying, started setting colors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-import './styles/navBar.css'
-import WebFont from 'webfontloader';
+import './styles/global.css';
+import './styles/navBar.css';
+import './styles/section-head.css'
+import './styles/section-about.css'
+import './styles/section-reviews.css'
+import './styles/section-contact.css'
+import './styles/footer.css'
+import './styles/app.css'
+
+/* import WebFont from 'webfontloader';
 
 WebFont.load({
     google: {
-      families: ['Exo', 'sans-serif'], // Reemplaza con las fuentes que elijas
+      families: ['Montserrat', 'sans-serif', 'system-ui'], // Reemplaza con las fuentes que elijas
     },
-});
+}); */
 
 
 
